539ab94e0245a9b857c97f3843f729085a17c5eb
sisa49 <sszypulski.kess@gmail.com>
2019-05-27 19:39:36 +0200

    Create Employee.cs
    Add Employee class to project.

---
+++ Manage Employees/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Employees
+{
+    namespace Emploees
+    {
+        class Employee : Person
+        {
+            public string WorkEmail { get; set; }
+            public string Position { get; set; }
+            public static decimal HolidayBonus { get; set; } = 1000;
+            public List<Operation> Operations { get; set; } = new List<Operation>();
+            public enum ContractTypes { FullTime, PartTime, Contract };
+
+            public Employee (string name, string surname, string workEmail, string position): base(name, surname)
+            {
+                WorkEmail = workEmail;
+                Position = position;
+            }
+        }
+
+    }
+    
+}
