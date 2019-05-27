5243e0dec3d8409ebf72aaa75cc58251a2d0f9b2
sisa49 <sszypulski.kess@gmail.com>
2019-05-27 19:38:40 +0200

    Small Updates in Program.cs and .csproj file

--- Manage Employees/Program.cs
+++ Manage Employees/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Manage_Employees.Clients;
+using Manage_Employees.Emploees;
 
 namespace Manage_Employees
-{
+{ 
     class Program
     {
         static void Main(string[] args)
         {
+            Client newClient = new Client("Jan", "Kowalski", "jan@onet.pl");
+            Messages.ColorPrint(newClient.GetClientData(),ConsoleColor.Blue);
+            Console.ReadKey();
         }
     }
 }
