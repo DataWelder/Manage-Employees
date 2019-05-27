eb8e0eb6f685c2853045356f5295f826839db584
sisa49 <sszypulski.kess@gmail.com>
2019-05-27 19:37:25 +0200

    Create Client.cs
    Add Client class to project.

---
+++ Manage Employees/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Manage_Employees.Emploees;
+
+namespace Manage_Employees
+{
+    namespace Clients
+    {
+        class Client : Person
+        {
+            public string Email {get; set;}
+
+            public Client (string name, string surname, string email): base(name, surname)
+            {
+                Email = email;
+            }
+
+            public string GetClientData()
+            {
+                return "Name: " + GetFullName() + "\nEmail: " + Email; 
+            }
+       }
+    }
+}
