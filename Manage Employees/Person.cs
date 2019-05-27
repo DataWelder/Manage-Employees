ff2d42ef05df0ad5195f66dc7508998ae493e604
sisa49 <sszypulski.kess@gmail.com>
2019-05-27 19:37:07 +0200

    Create Person.cs
    Add Person class to project.

---
+++ Manage Employees/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Employees
+{
+    namespace Emploees
+    {
+        abstract class Person
+        {
+            public string Name { get; set; }
+            public string Surname { get; set; }
+
+            public Person(string name, string surname)
+            {
+                Name = name;
+                Surname = surname;
+            }
+
+            public string GetFullName()
+            {
+                return Name + " " + Surname;
+            }
+        }
+    }
+}
