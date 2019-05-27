29d93359a06c3229c35a28bdb062bc03fe5902e3
sisa49 <sszypulski.kess@gmail.com>
2019-05-27 19:37:56 +0200

    Create Messages.cs
    Add Messages class to project with some methods.

---
+++ Manage Employees/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Employees
+{
+    class Messages
+    {
+        public static void ColorPrint(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
