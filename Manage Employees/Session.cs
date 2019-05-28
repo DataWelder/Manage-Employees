744904ce563799388e7be9066819deed49542773
Sisa <sszypulski.kess@gmail.com>
2019-05-28 19:31:14 +0200

    Create Session.cs

---
+++ Manage Employees/Session.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Employees
+{
+    class Session
+    {
+        private bool IsLogin = false;
+        private string Login = "admin";
+        private string Password = "admin";
+
+        public bool IsLogged()
+        {
+            return IsLogin;
+        }
+
+        public bool Loggin(string login, string password)
+        {
+            if(login == Login && password == Password)
+            {
+                IsLogin = true;
+                return true;
+            }
+            else
+            {
+                IsLogin = false;
+                return false;
+            }
+        }
+        public void Logout()
+        {
+            IsLogin = false;
+        }
+    }
+}
