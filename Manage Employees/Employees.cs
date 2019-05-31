48f04c4fc77e1112620dcba4abf715a90af9d671
Sisa <sszypulski.kess@gmail.com>
2019-05-31 12:30:38 +0200

    Create Employees class

---
+++ Manage Employees/Employees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manage_Employees
+{
+    namespace Emploees
+    {
+        class Employees
+        {
+            public List<Employee> EmployeesList { get; set; } = new List<Employee>();
+
+        }
+    }
+    
+}
